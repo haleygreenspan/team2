aeceee3e88b6e0264c43f325fc38e13e5fdb3c16
Antonio Pancorbo <apancorb@terpmail.umd.edu>
2021-01-06 18:20:10 -0500

    implemented move method

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -2,28 +2,24 @@ import java.util.HashMap;
 import java.util.HashSet;
 import javax.swing.JComponent;
 
-public class Map{
+public class Map {
 
 	public enum Type {
-		EMPTY,
-		PACMAN,
-		GHOST,
-		WALL,
-		COOKIE		
+		EMPTY, PACMAN, GHOST, WALL, COOKIE
 	}
-	
+
 	private HashMap<Location, HashSet<Type>> field;
 	private boolean gameOver;
 	private int dim;
 
 	private HashMap<String, Location> locations;
-	private HashMap<String, JComponent> components; 
+	private HashMap<String, JComponent> components;
 	private HashSet<Type> emptySet;
-	private HashSet<Type> wallSet; 
+	private HashSet<Type> wallSet;
 
 	private int cookies = 0;
 
-	public Map(int dim){
+	public Map(int dim) {
 		gameOver = false;
 		locations = new HashMap<String, Location>();
 		components = new HashMap<String, JComponent>();
@@ -36,41 +32,54 @@ public class Map{
 		this.dim = dim;
 	}
 
-
 	public void add(String name, Location loc, JComponent comp, Type type) {
 		locations.put(name, loc);
 		components.put(name, comp);
-		if (!field.containsKey(loc)) field.put(loc, new HashSet<Type>());
+		if (!field.containsKey(loc))
+			field.put(loc, new HashSet<Type>());
 		field.get(loc).add(type);
 	}
 
 	public int getCookies() {
 		return cookies;
 	}
-	
+
 	public boolean isGameOver() {
 		return gameOver;
 	}
-		
+
+	/*
+	 * The method takes a name, location, and type and put the object specified by
+	 * the name at the location. If the function is able to successfull move the
+	 * object it returns true, otherwise it returns false.
+	 */
+
 	public boolean move(String name, Location loc, Type type) {
-		//update locations, components, and field
-		//use the setLocation method for the component to move it to the new location
-		return false;
+		// update locations, components, and field
+		// use the setLocation method for the component to move it to the new location
+		try {
+			locations.put(name, loc);
+			components.get(name).setLocation(loc.x, loc.y);
+			field.get(loc).add(type);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
-	
+
 	public HashSet<Type> getLoc(Location loc) {
-		//wallSet and emptySet will help you write this method
+		// wallSet and emptySet will help you write this method
 		return null;
 	}
 
 	public boolean attack(String Name) {
-		//update gameOver
+		// update gameOver
 		return false;
 	}
-	
+
 	public JComponent eatCookie(String name) {
-		//update locations, components, field, and cookies
-		//the id for a cookie at (10, 1) is tok_x10_y1
+		// update locations, components, field, and cookies
+		// the id for a cookie at (10, 1) is tok_x10_y1
 		return null;
 	}
 }
