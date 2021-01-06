aef4e586bb3085a8302b9be87b58f6bc59bdb076
Antonio Pancorbo <apancorb@terpmail.umd.edu>
2021-01-06 17:56:36 -0500

    get_valid_moves for the ghost class

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.ArrayList;
 
-public class Ghost{
+public class Ghost {
 	String myName;
 	Location myLoc;
 	Map myMap;
@@ -12,15 +12,65 @@ public class Ghost{
 		this.myMap = map;
 	}
 
+	/*
+	 * This function returns an arraylist of possible locations that a ghost can
+	 * move to from its current location. Ghosts cannot move through walls, but they
+	 * can move through other ghosts or PacMan.
+	 */
+
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+		// instantiate the output array of valid moves
+		ArrayList<Location> validMoves = new ArrayList<Location>();
+
+		/* Horizontal and Vertical */
+
+		// check (x+1 , y)
+		if (!this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+		// check (x-1, y)
+		if (!this.myMap.getLoc(this.myLoc.x - 1, this.myLoc.y).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+		// check (x, y+1)
+		if (!this.myMap.getLoc(this.myLoc.x, this.myLoc.y + 1).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+		// check (x, y-1)
+		if (!this.myMap.getLoc(this.myLoc.x, this.myLoc.y - 1).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		/* Diagonally */
+
+		// check (x+1 , y+1)
+		if (!this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		// check (x-1 , y+1)
+		if (!this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		// check (x-1 , y-1)
+		if (!this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		// check (x+1 , y-1)
+		if (!this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y).contains(Map.Type.WALL)) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		return validMoves;
 	}
 
 	public boolean move() {
 		return false;
 	}
 
-	public boolean is_pacman_in_range() { 
+	public boolean is_pacman_in_range() {
 		return false;
 	}
 
