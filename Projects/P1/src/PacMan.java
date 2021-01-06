3e57b0d0b6e9204d4c8284a17f07207869d361f3
Bharath Hegde <bhegde8@gmail.com>
2021-01-06 17:16:13 -0500

    Is ghost in range initial code

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -22,7 +22,34 @@ public class PacMan{
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
+	/**
+	 * Determines whether there is a ghost within a 1 radius attack
+	 * range of pacman
+	 * 
+	 * @return true if a ghost is in range of pacman, and false if not
+	 */
+	public boolean is_ghost_in_range() {
+		// Iterate through all x coordinates within a radius of 1
+		for (int x = myLoc.x - 1; x <= myLoc.x + 1; x++) {
+			// Iterate through all y coordinates within a radius of 1
+			for (int y = myLoc.y - 1; y <= myLoc.y + 1; y++) {
+
+				// Get a HashSet of all the objects at the current
+				// (x, y) coordinates being checked
+				Location loc = new Location(x, y);
+
+				HashSet<Map.Type> objects = myMap.getLoc(loc);
+
+				// If there is a ghost at this location, then a ghost
+				// is in range of pacman
+				if (objects.contains(Map.Type.GHOST)) {
+					return true;
+				}
+			}
+		}
+
+		// If none of the coordinates checked had a ghost, there is no
+		// ghost in range of pacman
 		return false;
 	}
 
