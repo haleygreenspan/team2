82b1556bb39e0079de4ac33df5bf0e24e55cc571
Antonio Pancorbo <apancorb@terpmail.umd.edu>
2021-01-06 17:23:46 -0500

    added the function get valid moves

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -2,11 +2,11 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 
-public class PacMan{
+public class PacMan {
 	String myName;
 	Location myLoc;
 	Map myMap;
-	Location shift; 
+	Location shift;
 
 	public PacMan(String name, Location loc, Map map) {
 		this.myLoc = loc;
@@ -14,19 +14,67 @@ public class PacMan{
 		this.myMap = map;
 	}
 
+	/*
+	 * This method returns all the valid moves that PacMan can make given his
+	 * current position.
+	 */
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		// instantiate the output array of valid moves
+		ArrayList<Location> validMoves = new ArrayList<Location>();
+
+		/* Horizontal and Vertical */
+
+		// check (x+1 , y)
+		if (this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+		// check (x-1, y)
+		if (this.myMap.getLoc(this.myLoc.x - 1, this.myLoc.y) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+		// check (x, y+1)
+		if (this.myMap.getLoc(this.myLoc.x, this.myLoc.y + 1) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+		// check (x, y-1)
+		if (this.myMap.getLoc(this.myLoc.x, this.myLoc.y - 1) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		/* Diagonally */
+
+		// check (x+1 , y+1)
+		if (this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		// check (x-1 , y+1)
+		if (this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		// check (x-1 , y-1)
+		if (this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		// check (x+1 , y-1)
+		if (this.myMap.getLoc(this.myLoc.x + 1, this.myLoc.y) != Map.Type.WALL) {
+			validMoves.add(new Location(this.myLoc.x + 1, this.myLoc.y));
+		}
+
+		return validMoves;
 	}
 
 	public boolean move() {
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
+	public boolean is_ghost_in_range() {
 		return false;
 	}
 
-	public JComponent consume() { 
- 		return null;
+	public JComponent consume() {
+		return null;
 	}
 }
