a59114ca2d46df65f7826efa2047a083779359fd
Sruthi R <gogators91@gmail.com>
2021-01-06 18:06:41 -0500

    Added eatcookie functionality

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -69,6 +69,7 @@ public class Map{
 	}
 	
 	public JComponent eatCookie(String name) {
+		locations.put(name, )
 		//update locations, components, field, and cookies
 		//the id for a cookie at (10, 1) is tok_x10_y1
 		return null;
