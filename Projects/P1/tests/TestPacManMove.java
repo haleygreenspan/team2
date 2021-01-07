42087dab5cf3199a90023f7ac494dff2debcedc9
xuewei_ji <camelliaji2016@gmail.com>
2021-01-07 14:20:10 -0500

    implement TestPacManMove.java

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -3,8 +3,28 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestPacManMove extends TestCase {
-
-	public void testPacManMove() throws FileNotFoundException{
-		return null;
+	/*
+	@Before
+	public void setUp() throws Exception {
 	}
+   
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test */
+	public void testPacManMove() throws FileNotFoundException{
+		//Creating A Map 
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		//Creating a Player
+		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
+		assertEquals(true, pacman.move());
+	}
+   
+	/*
+	@Test
+	public void testPacManMove1() throws FileNotFoundException{
+	 
+	} */
+   
 }
