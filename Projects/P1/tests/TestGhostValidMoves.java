1a1017d8ef8278621af78402c019950ab2025b67
Antonio Pancorbo <apancorb@terpmail.umd.edu>
2021-01-07 16:38:42 -0500

    added test for ghost get valid moves

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -4,7 +4,13 @@ import java.io.*;
 
 public class TestGhostValidMoves extends TestCase {
 
-	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+	public void testGhostValidMoves() throws FileNotFoundException {
+		// Creating A Map
+		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
+
+		// A ghost at location 0,0 should be able to move somewhere since that is where
+		// the game starts
+		Ghost ghost1 = frame.addGhost(new Location(0, 0), "ghost1", Color.BLACK);
+		assertTrue(ghost1.get_valid_moves().size() != 0);
 	}
 }
