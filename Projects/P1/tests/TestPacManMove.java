fb6b8cf6623cef2192781cafe1bebe1965ffcd3b
xuewei_ji <camelliaji2016@gmail.com>
2021-01-07 14:26:50 -0500

    modified TestPacManMove.java

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -17,8 +17,12 @@ public class TestPacManMove extends TestCase {
 		//Creating A Map 
 		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
 		//Creating a Player
-		PacMan pacman = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
-		assertEquals(true, pacman.move());
+		PacMan pacman1 = frame.addPacMan(new Location(0, 0)); //Creates PacMan at location x, y
+		assertEquals(true, pacman1.move());
+		PacMan pacman2 = frame.addPacMan(new Location(-1, -1)); //Creates PacMan at location x, y
+		assertEquals(false, pacman2.move());
+		PacMan pacman3 = frame.addPacMan(new Location(40, 40)); //Creates PacMan at location x, y
+		assertEquals(false, pacman3.move());
 	}
    
 	/*
