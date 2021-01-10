1163bab8b0c82cf90f7ac63e7619924a121b05f2
Sruthi R <gogators91@gmail.com>
2021-01-10 16:58:04 -0500

    Added extends TestCase and FileNotFoundException

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -2,9 +2,9 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 import static org.junit.Assert.*;
-public class TestMapEatCookie {
+public class TestMapEatCookie extends TestCase{
 	
-	public void testMapEatCookie() {
+	public void testMapEatCookie() throws FileNotFoundException {
 		// Creating A Map
 		NoFrame frame = new NoFrame();
 
